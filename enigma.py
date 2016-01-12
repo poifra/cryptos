20474cd2d0edfa76034e53035a554078316500a6
Francois <fpoitras123@gmail.com>
2016-01-12 16:40:23 -0500

    started implementing rotors

--- enigma.py
+++ enigma.py
@@ -1,28 +1,35 @@
 import random
+from collections import deque
 
 ALPHABET = "abcdefghijklmnopqrstuvwxyz"
 
-class Rotor
+class Rotor:
 	def __init__(self,transitions = None):
 		if transitions is None:
-			self.transitions = dict()
-			for c in ALPHABET:
-				sub = random.choice(c)
-
-				while sub is c:
-					sub = random.choice(c)
-
-				transitions[c] = sub
+			self.transitions = deque(list(ALPHABET))
+			while self.transitions[0] is 'a':
+				self.transitions.rotate(random.randint(1,25))
 		else:
 			self.transitions = transitions
-
-	def getNextTransition(self):
-		pass
+		self.position = 0
+	def correspondance(self,letter):
+		return self.transitions[ALPHABET.index(letter)]
+	def shift(self):
+		self.transitions.rotate(1)
 
 class EnigmaKey:
 	def __init__(self):
-			self.rotor1 = Rotor()
-			self.rotor2 = Rotor()
-			self.rotor3 = Rotor()
+		self.rotor1 = Rotor()
+		self.rotor2 = Rotor()
+		self.rotor3 = Rotor()
 	def encrypt(self,letter):
-		pass
+		self.rotor1.shift()
+		self.rotor1.position += 1
+		if self.rotor1.position == 26:
+			self.rotor1.position = 0
+			self.rotor2.shift()
+			self.rotor2.position += 1
+			if self.rotor2.position == 26:
+				self.rotor2.position = 0
+				self.rotor3.shift()
+				self.rotor
